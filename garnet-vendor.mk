0f48c3328308f1e8dd3f3b18624e8136e72b91d1
AdarshGrewal <adarshgrewal@gmail.com>
2025-05-26 18:34:14 +0530

    garnet: Import missing AtFwd2 app
    Change-Id: Ic076ccd09816ac2977afb46ea6f500b2d1e70e6f

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -2029,6 +2029,7 @@ PRODUCT_PACKAGES += \
     EuiccGoogle \
     HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
+    AtFwd2 \
     ImsRcsService \
     PowerOffAlarm \
     QCC \
